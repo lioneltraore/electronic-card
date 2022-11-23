53846d51fff63d2e5d82f11b6db7e961f02aa870
Lionel Traore <hello@lioneltraore.com>
2022-11-23 18:57:47 +0000

    add text

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromRGBO(37, 48, 42, 100),
+        backgroundColor: Color.fromRGBO(37, 48, 42, 1),
         body: SafeArea(
           child: Column(
-            children: [
+            children: const [
               CircleAvatar(
                 backgroundImage: AssetImage("images/profile.png"),
-                radius: 50,
-              )
+                radius: 60,
+              ),
+              Text(
+                "LIONEL TRAORE",
+                style: TextStyle(
+                    color: Color.fromRGBO(252, 234, 206, 1),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Open Sans"),
+              ),
+              Text(
+                "WEB & MOBILE DEVELOPER",
+                style: TextStyle(
+                  color: Color.fromRGBO(252, 234, 206, 0.7),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "Open Sans",
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Color.fromRGBO(252, 234, 206, 0.4),
+                ),
+              ),
             ],
           ),
         ),
